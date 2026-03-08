44085d3e60b25bac5e29f6d58abcff3a4f5be5e7
Tharunkumar <tharunraji101@gmail.com>
2026-03-09 00:03:21 +0530

    Update setting

--- jobtracker/settings.py
+++ jobtracker/settings.py
@@ -19,13 +19,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
+import os
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-34#48@2@3$aiiq+$2$t&5rjc$$iph@&w31c^eugjqrigvsy18u'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-34#48@2@3$aiiq+$2$t&5rjc$$iph@&w31c^eugjqrigvsy18u')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
@@ -128,3 +131,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
